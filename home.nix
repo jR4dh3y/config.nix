63c2c51f7bdf7bb39bc8220197f55381a42cda96
jR4dh3y <radheykalra901@gmail.com>
2025-05-31 22:18:12 +0530

    rev2

--- home.nix
+++ home.nix
@@ -11,18 +11,25 @@
   # Home Manager dotfiles.
   home.file = {
 
-      ".config/anyrun" .source = ./cfg/anyrun;
-      ".config/hypr" .source = ./cfg/hypr;
-      ".config/rofi" .source = ./cfg/rofi;
-      ".config/wlogout" .source = ./cfg/wlogout;
-      ".config/waybar" .source = ./cfg/waybar;
-      ".config/wallust" .source = ./cfg/wallust;
-      ".local/bin/" .source = ./cfg/bin;
+      # ".config/anyrun" .source = ./cfg/anyrun;
+      # ".config/hypr" .source = ./cfg/hypr;
+      # ".config/rofi" .source = ./cfg/rofi;
+      # ".config/wlogout" .source = ./cfg/wlogout;
+      # ".config/waybar" .source = ./cfg/waybar;
+      # ".config/wallust" .source = ./cfg/wallust;
+      # ".local/bin/" .source = ./cfg/bin;
   };
 
   # Home Manager can also manage your environment variables.
   home.sessionVariables = {
     EDITOR = "code";
+  };
+
+
+  programs.git = {
+    enable = true;
+    userName  = "jR4dh3y";
+    userEmail = "radheykalra901@gmail.com";
   };
 
   # Let Home Manager install and manage itself.
